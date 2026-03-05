d432635b26e9e55f71b53c4d3b21ea3d6d4272d5
sandalu <sandaluthisankaa@gmail.com>
2026-03-05 12:39:38 +0530

    test

--- api/config/cors.php
+++ api/config/cors.php
@@ -7,6 +7,7 @@ $allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
+  "http://localhost:5176",
   "http://localhost:3000",
   "http://localhost:4173",
 ];
